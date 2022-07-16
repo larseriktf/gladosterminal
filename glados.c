62af1a263d8d2424a9a8aca96a5f3d0142f1f067
larseriktf <github@larserik.xyz>
2022-07-16 19:49:49 +0200

    Added dynamically allocated lines array

--- glados.c
+++ glados.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define BUFFER_SIZE 4096
 
 int delay(int ms) {
 	usleep(ms * 1000);
@@ -7,23 +11,44 @@ int delay(int ms) {
 
 int play_song(FILE *stream)
 {
-	char lyrics[1000];
-	int bpm = 120;
-	int num_beats = 4;
-	int num_notes = 4;
-	int duration = 186000; // 3 minutes to milliseconds
+	char **lines;
+	char buffer[BUFFER_SIZE];
+	int n = 0;
 
-	while (fgets(lyrics, 1000, stream) != NULL)
+	// Get number of strings from first line
+	fgets(buffer, BUFFER_SIZE, stream);	
+	n = atoi(buffer);
+
+	lines = malloc(n * sizeof(char *));
+	int length = 0;
+
+	for (int i = 0; i < n; i++)
 	{
-		printf("%s", lyrics);
-		delay(50);
+		fgets(buffer, BUFFER_SIZE, stream);
+		length = strlen(buffer);
+
+		// Eliminate \n character at the end of each string
+		buffer[length - 1] = '\0';
+
+		// Fill lines array
+		lines[i] = malloc(length * sizeof(char));
+		strcpy(lines[i], buffer);
 	}
+
+	// Print each line
+	for (int i = 0; i < n; i++)
+		printf("%s\n", lines[i]);
+
+	// Free space
+	for (int i = 0; i < n-1; i++)
+		free(lines[i]);	
+	free(lines);
 }
 
 int main()
 {
 	FILE *stream = NULL;
-	stream = fopen("lyrics.txt", "r");
+	stream = fopen("lyrics.csv", "r");
 
 	if (!stream)
 	{
