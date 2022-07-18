1b3d55eb3405ac419ea5e7d90a3908626cf51042
larseriktf <github@larserik.xyz>
2022-07-18 17:40:34 +0200

    Extracted lines generation to method

--- glados.c
+++ glados.c
@@ -2,45 +2,23 @@
 #include <unistd.h>
 #include <string.h>
 #include <stdlib.h>
+#include "utility.h"
 
-#define BUFFER_SIZE 4096
-
-int delay(int ms) {
+void delay(int ms) {
 	usleep(ms * 1000);
 }
 
 int play_song(FILE *stream)
 {
-	char **lines;
-	char buffer[BUFFER_SIZE];
 	int n = 0;
-
-	// Get number of strings from first line
-	fgets(buffer, BUFFER_SIZE, stream);	
-	n = atoi(buffer);
-
-	lines = malloc(n * sizeof(char *));
-	int length = 0;
-
-	for (int i = 0; i < n; i++)
-	{
-		fgets(buffer, BUFFER_SIZE, stream);
-		length = strlen(buffer);
-
-		// Eliminate \n character at the end of each string
-		buffer[length - 1] = '\0';
-
-		// Fill lines array
-		lines[i] = malloc(length * sizeof(char));
-		strcpy(lines[i], buffer);
-	}
+	char **lines = get_lines(&n, stream);
 
 	// Print each line
 	for (int i = 0; i < n; i++)
 		printf("%s\n", lines[i]);
 
 	// Free space
-	for (int i = 0; i < n-1; i++)
+	for (int i = 0; i < n; i++)
 		free(lines[i]);	
 	free(lines);
 }
@@ -57,8 +35,6 @@ int main()
 	}
 
 	play_song(stream);
-
 	fclose(stream);
-
 	return 0;
 }
---
+++ utility.h
@@ -0,0 +1,26 @@
+#define BUFFER_SIZE 4096
+
+char **get_lines(int *n, FILE *stream)
+{
+	char **lines;
+	char buffer[BUFFER_SIZE];
+	int length = 0;
+
+	// Read and assign n lines
+	fgets(buffer, BUFFER_SIZE, stream);	
+	*n = atoi(buffer);
+
+	lines = malloc(*n * sizeof(char *));
+
+	// Fill lines and eliminate \n character
+	for (int i = 0; i < *n; i++)
+	{
+		fgets(buffer, BUFFER_SIZE, stream);
+		length = strlen(buffer);
+		buffer[length - 1] = '\0';
+		lines[i] = malloc(length * sizeof(char));
+		strcpy(lines[i], buffer);
+	}
+
+	return lines;
+}
