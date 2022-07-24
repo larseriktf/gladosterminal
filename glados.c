528155645357c34b81e607ce2824a9864260baa7
larseriktf <github@larserik.xyz>
2022-07-24 23:41:04 +0200

    Reworked print_line_animated to print beginning unanimated

--- glados.c
+++ glados.c
@@ -5,33 +5,50 @@
 #include "utility.h"
 #include "tc.h"
 
-void print_line_animated(int length, char *line, int ms)
-{
-		// Print lines gradually with delay to create an animated line
-		for (int i = 0; i < length; i++)
-		{
-			delay(ms);
-			printf("\r");
-			for (int j = 0; j <= i; j++)
-				printf("%c", line[j]);
-			fflush(stdout);
-		}
-}
-
-void play_song(FILE *stream)
+void play_song(FILE *stream, int bpm)
 {
 	int n = 0;
 	int letters = 0;
 	char **lines = get_lines(&n, stream);
 
 	// Print each line
-	for (int i = 0; i < 10; i++)
+	/*
+	for (int i = 0; i < n; i++)
 	{
-		letters = strlen(lines[i]);
-		print_line_animated(letters, lines[i], 50);
+		char *line = lines[i];
+		char *str_piece;
+		int beats = 0;
+		int start = 0;
+		int end = 0;
+
+		if (i % 2 == 0)
+		{
+			// Even: Print line
+			str_piece = strtok(line, ";");
+			end += strlen(str_piece);
+			while (str_piece != NULL)
+			{
+				print_line_animated(start, end, line, 50);
+				start += strlen(str_piece);
+				str_piece = strtok(NULL, ";");
+				end += strlen(str_piece);
+			}
+		}
+		else
+		{
+			// Odd: Read beats
+			str_piece = strtok(line, ";");
+			while (str_piece != NULL)
+			{
+				letters = strlen(str_piece);
+				str_piece = strtok(NULL, ";");
+			}
+		}
+
 		printf("\n");
-		delay(50);
 	}
+	*/
+	print_line_animated(6, 17, lines[1], 50);
 
 	// Free lines
 	for (int i = 0; i < n; i++)
@@ -50,7 +67,7 @@ int main()
 		return 1;
 	}
 
-	play_song(stream);
+	play_song(stream, 120);
 	fclose(stream);
 	return 0;
 }
--- utility.h
+++ utility.h
@@ -8,13 +8,13 @@ char **get_lines(int *n, FILE *stream)
 	char buffer[BUFFER_SIZE];
 	int length = 0;
 
-	// Read and assign n lines
+	// Read and assign n from first line in stream
 	fgets(buffer, BUFFER_SIZE, stream);	
 	*n = atoi(buffer);
 
 	lines = malloc(*n * sizeof(char *));
 
-	// Fill lines and eliminate \n character
+	// Fill lines array and eliminate \n character
 	for (int i = 0; i < *n; i++)
 	{
 		fgets(buffer, BUFFER_SIZE, stream);
@@ -25,4 +25,19 @@ char **get_lines(int *n, FILE *stream)
 	}
 
 	return lines;
+}
+
+void print_line_animated(int start, int end, char *line, int ms)
+{
+		// Print lines gradually with delay to animate line
+		for (int i = start; i < end; i++)
+		{
+			delay(ms);
+			printf("\r");
+		// Print beginning unanimated
+			printf("%*.*s", start, start, line);
+			for (int j = start; j <= i; j++)
+				printf("%c", line[j]);
+			fflush(stdout);
+		}
 }
