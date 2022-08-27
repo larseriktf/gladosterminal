bb4b62e8a8a3420a8759a4a6c1a6dad75cb7626f
larseriktf <github@larserik.xyz>
2022-08-27 18:28:25 +0200

    Lyrics now prints correctly

--- glados.c
+++ glados.c
@@ -13,14 +13,14 @@
 
 /* Function Declaration */
 
-char **get_lines(int *n, FILE *stream);
 void str_replace_index(char *str, const char character, int start, int end);
 char *str_copy_index(char *dest, const char *src, int start, int end, int size);
 void delay(int ms);
-void print_line_animated(int length, char *line);
-void play_song(FILE *stream, int bpm);
+void print_line_animated(int length, char *line, int x, int y);
+void play_song(char **lines, int n);
 void draw_column(int max_w, int max_h, int l_margin, int v_padding);
 void draw();
+char **get_lines(int *n, FILE *stream);
 int main();
 
 /* Function Implementation */
@@ -32,6 +32,7 @@ int main()
 	enter_screen();
 	echo_off();
 	hide_cursor();
+	printf("%s%s", COLOR_FG, COLOR_BG);
 
 	// Load lyrics file
 	FILE *stream = NULL;
@@ -43,9 +44,11 @@ int main()
 		return 1;
 	}
 
-	draw();
+	int n = 0;
+	char **lines = get_lines(&n, stream);
 
-	// play_song(stream, 120);
+	draw();
+	play_song(lines, n);
 
 	// Keep application running
 	char c = getchar();
@@ -55,93 +58,9 @@ int main()
 	exit_screen();
 	echo_on();
 	show_cursor();
+	printf("%s", COLOR_NRM);
 
 	return 0;
-}
-
-void draw()
-{
-	// Basic settings
-	int rows = 0, cols = 0;
-	get_rows_cols(&rows, &cols);
-	clear_screen();
-	printf("%s%s", COLOR_FG, COLOR_BG);
-
-	int col1_min_w = 31, col1_max_w = 98;
-	int col1_min_h = 20, col1_max_h = 64;
-	int col2_min_w = 31, col2_max_w = 71;
-	int col2_min_h = 10, col2_max_h = 20;
-
-	int col1_w = col1_max_w, col1_h = col1_max_h;
-	int col2_w = col2_max_w, col2_h = col2_max_h;
-
-	int spacing = 2;
-
-	// Actual drawing
-	// Fill background with black
-	for (int i = 0; i < rows; i++)
-	{
-		for (int k = 0; k < cols; k++) printf(" ");
-		printf("\n");
-	}
-
-	// Draw column 1 and 2
-	draw_column(col1_w, col1_h, 0, 0);
-	//draw_column(col2_w, col2_h, col1_w + spacing, 2);
-
-	// Reset effects
-	printf("%s", COLOR_NRM);
-}
-
-void draw_column(int max_w, int max_h, int l_margin, int v_padding)
-{
-	int i = 0;
-	for (i = 0 + v_padding; i < max_w - v_padding; i++)
-	{
-		if (i % 2 == 0)
-		{
-			move_cursor(i + l_margin, 0);
-			printf("_");
-			move_cursor(i + l_margin, max_h-1);
-			printf("_");
-		}
-	}
-	for (i = 1; i < max_h-1; i++)
-	{
-		if (i % 2 == 0)
-		{
-			move_cursor(l_margin, i);
-			printf("|");
-			move_cursor(l_margin + max_w - 1, i);
-			printf("|");
-		}
-	}
-}
-
-void play_song(FILE *stream, int bpm)
-{
-	int n = 0;
-	int letters = 0;
-	char **lines = get_lines(&n, stream);
-
-	printf("\033[38;2;255;0;0m");
-	printf("\033[48;2;0;0;0m");
-
-	// Print each line
-	for (int i = 0; i < n; i++)
-	{
-		char *line = lines[i];
-
-		letters = strlen(line);
-		print_line_animated(letters, line);
-	}
-
-	printf("\033[0m\n");
-
-	// Free lines
-	for (int i = 0; i < n; i++)
-		free(lines[i]);	
-	free(lines);
 }
 
 char **get_lines(int *n, FILE *stream)
@@ -169,7 +88,80 @@ char **get_lines(int *n, FILE *stream)
 	return lines;
 }
 
-void print_line_animated(int length, char *line)
+void draw()
+{
+	// Basic settings
+	int rows = 0, cols = 0;
+	get_rows_cols(&rows, &cols);
+
+	int col1_min_w = 31, col1_max_w = 98;
+	int col1_min_h = 20, col1_max_h = 64;
+	int col2_min_w = 31, col2_max_w = 71;
+	int col2_min_h = 10, col2_max_h = 20;
+
+	int col1_w = col1_max_w, col1_h = col1_max_h;
+	int col2_w = col2_max_w, col2_h = col2_max_h;
+
+	int spacing = 2;
+
+	// Actual drawing
+	// Fill background with black
+	for (int i = 0; i < rows; i++)
+	{
+		for (int k = 0; k < cols; k++) printf(" ");
+		printf("\n");
+	}
+
+	// Draw column 1 and 2
+	draw_column(col1_w, col1_h, 0, 0);
+	//draw_column(col2_w, col2_h, col1_w + spacing, 2);
+}
+
+void draw_column(int max_w, int max_h, int l_margin, int v_padding)
+{
+	int i = 0;
+	for (i = 0 + v_padding; i < max_w - v_padding; i++)
+	{
+		if (i % 2 == 0)
+		{
+			move_cursor(i + l_margin, 0);
+			printf("_");
+			move_cursor(i + l_margin, max_h-1);
+			printf("_");
+		}
+	}
+	for (i = 1; i < max_h-1; i++)
+	{
+		if (i % 2 == 0)
+		{
+			move_cursor(l_margin, i);
+			printf("|");
+			move_cursor(l_margin + max_w - 1, i);
+			printf("|");
+		}
+	}
+}
+
+void play_song(char **lines, int n)
+{
+	int letters = 0;
+	// Print each line
+	for (int i = 0; i < n; i++)
+	{
+		char *line = lines[i];
+		letters = strlen(line);
+
+		print_line_animated(letters, line, 2, i + 1);
+	}
+
+	// Free lines
+	for (int i = 0; i < n; i++)
+		free(lines[i]);	
+	free(lines);
+}
+
+
+void print_line_animated(int length, char *line, int x, int y)
 {
 	bool print = false;	
 	char buffer[4] = {'\0'};
@@ -192,7 +184,7 @@ void print_line_animated(int length, char *line)
 		}
 		else if (print)
 		{
-			// If buffer is empty, prepare for printing
+			// If buffer is empty, read number and prepare for printing
 			if (buffer[0] == '\0')
 			{
 				str_copy_index(buffer, line, start + 1, end - 1, 4);
@@ -201,18 +193,18 @@ void print_line_animated(int length, char *line)
 			}
 
 			// Gradually print message with delay
-			delay(ms);
-			printf("\r");
-			for (int j = 0; j <= i; j++)
+			if (line[i] == '~') continue;
+			if (line[i] == '\\') printf("");
+			else
 			{
-				if (line[j] == '~') continue;
-				if (line[j] == '\\') printf("");
-				else printf("%c", line[j]);
+				move_cursor(x, y);
+				printf("%c", line[i]);
+				x++;
 			}
+			delay(ms);
 			fflush(stdout);
 		}
 	}
-	printf("\n");
 }
 
 void delay(int ms) { usleep(ms * 1000); }
