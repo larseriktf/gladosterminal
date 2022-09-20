35263c0fabe4203a3f067735591f49e2c350a3a0
larseriktf <github@larserik.xyz>
2022-09-20 22:53:44 +0200

    Added draw_ascii_art()

--- glados.c
+++ glados.c
@@ -22,8 +22,14 @@
 // #define CREDITS_X1 102
 // #define CREDITS_Y1 18
 
+#define ASCII_X0 60
+#define ASCII_Y0 20
+#define ASCII_X1 100
+#define ASCII_Y1 40
+
 /* Function Declaration */
 
+void draw_ascii_art(FILE *stream);
 void play_song(FILE *stream);
 void draw_border(FILE *file);
 int main();
@@ -40,14 +46,15 @@ int main()
 
 	FILE *border_stream = fopen("border.txt", "r");
 	FILE *lyrics_stream = fopen("lyrics.txt", "r");
-	FILE *images_stream = fopen("ascii_art.txt", "r");
+	FILE *ascii_stream = fopen("ascii_art.txt", "r");
 
 	if (!border_stream) return error("error opening border.txt\n");
 	if (!lyrics_stream) return error("error opening lyrics.txt\n");
-	if (!images_stream) return error("error opening ascii_art.txt\n");
+	if (!ascii_stream) return error("error opening ascii_art.txt\n");
 
 	draw_border(border_stream);
-	play_song(lyrics_stream);
+	//play_song(lyrics_stream);
+	draw_ascii_art(ascii_stream);
 
 	// Keep application running until keypress
 	char c = getchar();
@@ -56,7 +63,7 @@ int main()
 
 	fclose(border_stream);
 	fclose(lyrics_stream);
-	fclose(images_stream);
+	fclose(ascii_stream);
 
 	exit_screen();
 	echo_on();
@@ -67,7 +74,7 @@ int main()
 	return 0;
 }
 
-void draw_border(FILE *file)
+void draw_border(FILE *stream)
 {
 	char c;
 	int rows = 0, cols = 0;
@@ -77,7 +84,7 @@ void draw_border(FILE *file)
 	move_cursor(0, 0);
 
 	// Draw border from file
-	while((c = fgetc(file)) != EOF)
+	while((c = fgetc(stream)) != EOF)
 		printf("%c", c);
 }
 
@@ -121,6 +128,49 @@ void play_song(FILE *stream)
 			int ic = c - '0';
 			ms = ms * 10;
 			ms = ms + ic;
+		}
+	}
+}
+
+void draw_ascii_art(FILE *stream)
+{
+	char c;
+	int ms = 0, count = 20;
+	int x = ASCII_X0, y = ASCII_Y0;
+
+	while((c = fgetc(stream)) != EOF)
+	{
+		if (count == 20)
+		{
+			if (c == '\n')
+			{
+				delay(ms);
+				y = ASCII_Y0;
+				ms = 0;
+				count = 0;
+				clear(ASCII_X0, ASCII_Y0, ASCII_X1, ASCII_Y1);
+				continue;
+			}
+
+			int ic = c - '0';
+			ms = ms * 10;
+			ms = ms + ic;
+		}
+		else
+		{
+			if (c == '\n')
+			{
+				y++;
+				x = ASCII_X0;
+				count++;
+				continue;
+			}
+
+			//delay(ms);
+			move_cursor(x, y);
+			printf("%c", c);
+			fflush(stdout);
+			x++;
 		}
 	}
 }
